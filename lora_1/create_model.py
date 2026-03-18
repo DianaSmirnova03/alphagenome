f0df8f8b91a07848a3590154621409020ac1d726
DianaSmirnova03 <115831360+DianaSmirnova03@users.noreply.github.com>
2026-03-18 15:45:03 +0300

    Update create_model.py

--- lora_1/create_model.py
+++ lora_1/create_model.py
@@ -4,14 +4,7 @@ from alphagenome_ft import create_model_with_heads, parameter_utils
 from register_head import *
 
 def get_model(model_version='all_folds', head_name='promoter_effect_head', device=None):
-    if device is None:
-        try:
-            device = jax.devices('gpu')[0]
-        except:
-            try:
-                device = jax.devices('tpu')[0]
-            except:
-                device = jax.devices('cpu')[0]
+    device = jax.devices()[0]
     model = create_model_with_heads(
         model_version,
         heads=[head_name],
